a8fce6fd43ab5636625c660df6f9c444f05faccb
Elektromannen <Ottarmr@stud.ntnu.no>
2025-10-07 16:25:54 +0200

    Added basic controller button support and restructured the GUI drawing to make it draw consistently

--- Firmware/Controller/Input.py
+++ Firmware/Controller/Input.py
@@ -1,2 +1,62 @@
 import pygame
+import time
 
+pygame.init()
+pygame.joystick.init()
+def controller_init():
+    # List the available joystick devices
+    global controller
+    controller = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+
+    # Initialize all detected joysticks
+    for controller in controller:
+        print(f"Joystick {controller.get_instance_id()}: {controller.get_name()}")
+
+times = 0
+# Example: Print the position of the left and right analog sticks for all detected joysticks
+"""
+while True:
+    pygame.event.get()
+    if event.type == joystick.
+    #Switch pro controller buttons
+    if joystick.get_button(0):
+        print("Button A pushed")
+    if joystick.get_button(1):
+        print("Button B pushed")
+    if joystick.get_button(2):
+        print("Button X pushed")
+    if joystick.get_button(3):
+        print("Button Y pushed")
+    if joystick.get_button(4):
+        print("Button - pushed")
+    if joystick.get_button(5):
+        print("Button Home pushed")
+    if joystick.get_button(6):
+        print("Button + pushed")
+    if joystick.get_button(7):
+        print("Button L3 pushed")
+    if joystick.get_button(8):
+        print("Button R3 pushed")
+    if joystick.get_button(9):
+        print("Bumper L pushed")
+    if joystick.get_button(10):
+        print("Bumper R pushed")
+    if joystick.get_button(11):
+        print("Dpad up pushed")
+    if joystick.get_button(12):
+        print("Dpad down pushed")
+    if joystick.get_button(13):
+        print("Dpad Left pushed")
+    if joystick.get_button(14):
+        print("Dpad right pushed")
+    if joystick.get_button(15):
+        print("Button Screenshot pushed")
+"""
+
+    #for joystick in joysticks:
+     #   left_x_axis = joystick.get_axis(0)
+      #  left_y_axis = joystick.get_axis(1)
+       # right_x_axis = joystick.get_axis(2)
+        #right_y_axis = joystick.get_axis(3)
+        #print(f"Left Joystick: X={left_x_axis}, Y={left_y_axis}, Right Joystick: X={right_x_axis}, Y={right_y_axis}")
+        #time.sleep(1)
--- Firmware/Controller/gui.py
+++ Firmware/Controller/gui.py
@@ -1,9 +1,12 @@
 import pygame
 import pygame_gui
 import math
-import os
+import Input
 
 pygame.init()
+pygame.joystick.init()
+Input.controller_init()
+from Input import controller
 
 thrust = 00
 heading = 0
@@ -16,6 +19,8 @@ prev_current = 0
 
 gauge_text = pygame.font.SysFont("Arial", 16)
 
+text_gui = False
+Gui_button_text = "Text GUI"
 pygame.display.set_caption("Drone GUI")
 
 window_surface = pygame.display.set_mode((800, 600))
@@ -48,43 +53,74 @@ background.fill(pygame.Color("#C0C0C0"))
 manager = pygame_gui.UIManager((800, 600))
 
 #Flight path
-pygame.draw.rect(background,"#13C76D", [200, 100, 400, 350],0)
+
 
 
 #Gauges
 thrust_text = gauge_text.render("Thrust power", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-thruster_gauge = pygame.image.load('Firmware/Controller/Thrust-gauge.png')
-pygame.draw.rect(background, (255, 0, 0), [700, 320, 40, 20], 0)
-background.blit(thruster_gauge, (700,100))
-background.blit(thrust_text, (672, 75))
-
+thruster_gauge = pygame.image.load('Firmware/Controller/Gui elements/Thrust-gauge.png')
 
 pitch_text = gauge_text.render("Pitch", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-pitch_gauge = pygame.image.load('Firmware/Controller/Gauge_template_empty.png')
-background.blit(pitch_gauge, (35,50))
-pitch_gauge = pygame.image.load('Firmware/Controller/Gauge_pitch_text.png')
-background.blit(pitch_gauge, (35,50))
-background.blit(pitch_text, (78, 30))
+pitch_gauge_back = pygame.image.load('Firmware/Controller/Gui elements/Gauge_template_empty.png')
+pitch_gauge = pygame.image.load('Firmware/Controller/Gui elements/Gauge_pitch_text.png')
 
 roll_text = gauge_text.render("Roll", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-roll_gauge = pygame.image.load('Firmware/Controller/Gauge_template_empty.png')
-background.blit(roll_gauge, (35,200))
-roll_gauge = pygame.image.load('Firmware/Controller/Gauge_rol_text.png')
-background.blit(roll_gauge, (35,200))
-background.blit(roll_text, (80, 180))
+roll_gauge_back = pygame.image.load('Firmware/Controller/Gui elements/Gauge_template_empty.png')
+roll_gauge = pygame.image.load('Firmware/Controller/Gui elements/Gauge_rol_text.png')
 
 heading_text = gauge_text.render("Heading", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-heading_gauge = pygame.image.load('Firmware/Controller/Gauge_template_empty.png')
-background.blit(heading_gauge, (35,350))
-heading_gauge = pygame.image.load('Firmware/Controller/Gauge_heading_text.png')
-background.blit(heading_gauge, (35,350))
-background.blit(heading_text, (67, 330))
+heading_gauge_back = pygame.image.load('Firmware/Controller/Gui elements/Gauge_template_empty.png')
+heading_gauge = pygame.image.load('Firmware/Controller/Gui elements/Gauge_heading_text.png')
+
+battery_text = gauge_text.render("Battery status:", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
+battery_voltage = gauge_text.render(f"Voltage: {voltage}V", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
+battery_current = gauge_text.render(f"Current: {current}A", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
 
 #Bottom button creation
 hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0, 500), (270, 100)), text='Thruster speed', manager=manager)
-camera_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((265, 500), (273, 100)), text="Toggle Camera", manager=manager)
+Gui_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((265, 500), (273, 100)), text=Gui_button_text, manager=manager)
 controll_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((530, 500), (273, 100)), text='Toggle Controller', manager=manager)
+    
 
+#Draw gauges
+def draw_gauges():
+    background.blit(pitch_gauge_back, (35,50))
+    background.blit(pitch_gauge, (35,50))
+    background.blit(pitch_text, (79,30))
+
+    background.blit(roll_gauge_back, (35,200))
+    background.blit(roll_gauge, (35,200))
+    background.blit(roll_text, (80, 180))
+
+    background.blit(heading_gauge_back, (35,350))
+    global heading
+    if heading >=0:
+        draw_wedge(background,(95, 410),59,heading,(255,0,0))
+        if heading >= +180:
+            heading -=359
+    else:
+        draw_wedge(background,(95, 410),59,heading,(255,0,0))
+        if  heading <= -180:
+            heading +=359
+    background.blit(heading_gauge, (35,350))
+    background.blit(heading_text, (67, 330))
+
+    pygame.draw.rect(background, (255, 0, 0), [700, 320, 40, 20], 0)
+    max_height = 335
+    bar_bottom_y = 340  # bottom of the gauge
+    # calculate the current height of the thrust bar based on thrust value
+    current_height = min(thrust, max_height)
+    # calculate the top-left y coordinate so it grows upwards
+    top_y = bar_bottom_y - current_height
+    # draw the rectangle
+    pygame.draw.rect(background, (255, 0, 0), [700, top_y-20, 40, current_height], 0)
+    background.blit(thruster_gauge, (700,100))
+    background.blit(thrust_text, (672, 75))
+
+def draw_battery_info():
+    background.blit(battery_text, (670, 430))
+    background.blit(battery_voltage, (670, 450))
+    background.blit(battery_current, (670, 470))
 
 
 clock = pygame.time.Clock()
@@ -94,48 +130,25 @@ is_running = True
 while is_running:
     time_delta = clock.tick(60)/1000.0
 
-    #Gauge logic
-   
-    heading_gauge = pygame.image.load('Firmware/Controller/Gauge_template_empty.png')
-    background.blit(heading_gauge, (35,350))
-    if heading >=0:
-        draw_wedge(background,(95, 410),59,heading,(255,0,0))
-        if heading >= +180:
-            heading -=359
+    #Initial fram draw
+    pygame.draw.rect(background,"#C0C0C0", [0, 0, 800, 600],0)
+    if (text_gui):
+        draw_battery_info()
     else:
-        draw_wedge(background,(95, 410),59,heading,(255,0,0))
-        if heading <= -180:
-            heading +=359
-    heading_gauge = pygame.image.load('Firmware/Controller/Gauge_heading_text.png')
-    background.blit(heading_gauge, (35,350))
-  
+        draw_gauges()
+        draw_battery_info()
 
-    battery_text = gauge_text.render("Battery status:", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-    battery_voltage = gauge_text.render(f"Voltage: {voltage}V", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-    battery_current = gauge_text.render(f"Current: {current}A", True, (0, 0, 0)) # Text, Antialiasing, Color (RGB)
-    
-    
-    pygame.draw.rect(background, ("#C0C0C0"), [200, 30, 110, 60], 0)
-    background.blit(battery_text, (200, 30))
-    background.blit(battery_voltage, (200, 50))
-    background.blit(battery_current, (200, 70))
-    
-    max_height = 335
-    bar_bottom_y = 340  # bottom of the gauge
-    # calculate the current height of the thrust bar based on thrust value
-    current_height = min(thrust, max_height)
-    # calculate the top-left y coordinate so it grows upwards
-    top_y = bar_bottom_y - current_height
-    # draw the rectangle
-    pygame.draw.rect(background, ("#C0C0C0"), [700, 320, 40, 0], 0)
-    pygame.draw.rect(background, (255, 0, 0), [700, top_y-20, 40, current_height], 0)
-    background.blit(thruster_gauge, (700,100))
+    #Continous controller handler
+    if controller.get_button(9): #Bumper L pushed
+                heading -= 20 * time_delta
+    if controller.get_button(10): #Bumper R pushed
+                heading += 20 * time_delta
 
-
-
+    #Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+        
         #Keypress events
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
@@ -156,10 +169,10 @@ while is_running:
                 elif event.key == pygame.K_q:
                     heading -=10
                     print(f"Turning left!\nHeading is now {heading}")
-                elif event.key == pygame.K_UP:
+                elif (event.key == pygame.K_UP and thrust < 200):
                     thrust += 1
                     print(f"Thrust up!\nThrust is now {thrust}")
-                elif event.key == pygame.K_DOWN:
+                elif (event.key == pygame.K_DOWN and thrust > 0):
                     thrust -= 1
                     print(f"Thrust down!\nThrust is now {thrust}")
         
@@ -167,10 +180,48 @@ while is_running:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == hello_button:
                 thrust = thrust + 10
-            if event.ui_element == camera_button:
-                print('Camera is on')
+            if event.ui_element == Gui_button:
+                text_gui = not text_gui
+                if (text_gui):
+                    Gui_button.set_text("Visual GUI")
+                else:
+                    Gui_button.set_text("Text GUI")
+                print(f"Gui switch is pressed and text state is {text_gui}")
             if event.ui_element == controll_button:
                 print('Controller enabled')
+
+
+        #Controller events
+        if event.type == pygame.JOYBUTTONDOWN:
+            #Switch pro controller buttons
+            if controller.get_button(0):
+                print("Button A pushed")
+            if controller.get_button(1):
+                print("Button B pushed")
+            if controller.get_button(2):
+                print("Button X pushed")
+            if controller.get_button(3):
+                print("Button Y pushed")
+            if controller.get_button(4):
+                print("Button - pushed")
+            if controller.get_button(5):
+                print("Button Home pushed")
+            if controller.get_button(6):
+                print("Button + pushed")
+            if controller.get_button(7):
+                print("Button L3 pushed")
+            if controller.get_button(8):
+                print("Button R3 pushed")
+            if controller.get_button(11): #Dpad up pushed
+                thrust += 1
+            if controller.get_button(12):#Dpad down pushed
+                thrust -= 1
+            if controller.get_button(13):#Dpad Left pushed
+                heading -= 5
+            if controller.get_button(14):#Dpad Right pushed
+                heading += 5
+            if controller.get_button(15):
+                print("Button Screenshot pushed")
 
 
         manager.process_events(event)
