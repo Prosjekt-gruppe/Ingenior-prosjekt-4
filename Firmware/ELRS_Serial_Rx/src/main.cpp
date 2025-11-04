71290cea685e82f9617577c237fdf0f562edb43e
Youmayu <103046178+Youmayu@users.noreply.github.com>
2025-11-04 13:58:36 +0100

    Added random telemetry and full duplex

--- Firmware/ELRS_Serial_Rx/src/main.cpp
+++ Firmware/ELRS_Serial_Rx/src/main.cpp
@@ -5,12 +5,15 @@ const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
 
 WebServer server(80);
-String lastValue = "No data yet";
+
+String incoming = "";
+String lastControl = "No control data yet";
+unsigned long lastTelemetry = 0;
 
 void handleRoot() {
   String html = "<html><head><meta http-equiv='refresh' content='1'></head><body>";
-  html += "<h1>ESP32 Serial Data</h1>";
-  html += "<p>Latest value: <b>" + lastValue + "</b></p>";
+  html += "<h1>ESP32 Drone Telemetry Server</h1>";
+  html += "<p><b>Last Control Input:</b> " + lastControl + "</p>";
   html += "</body></html>";
   server.send(200, "text/html", html);
 }
@@ -18,13 +21,15 @@ void handleRoot() {
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
+
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println();
-  Serial.print("Connected! IP: ");
+  Serial.print("Connected! IP Address: ");
   Serial.println(WiFi.localIP());
 
   server.on("/", handleRoot);
@@ -34,9 +39,28 @@ void setup() {
 void loop() {
   server.handleClient();
 
-  if (Serial.available()) {
-    lastValue = Serial.readStringUntil('\n');
-    lastValue.trim();
-    Serial.println("Received from Python: " + lastValue);
+  // ---- RECEIVE CONTROL INPUTS ----
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') {
+      incoming.trim();
+      lastControl = incoming;
+      Serial.print("Received control: ");
+      Serial.println(incoming);
+      incoming = "";
+    } else {
+      incoming += c;
+    }
+  }
+
+  // ---- SEND TELEMETRY ----
+  if (millis() - lastTelemetry > 100) { // every 100 ms
+    lastTelemetry = millis();
+    float altitude = random(100, 200) / 1.0;
+    float speed = random(0, 50) / 1.0;
+    float battery = random(70, 100) / 1.0;
+
+    String telemetry = "ALT:" + String(altitude) + ",SPD:" + String(speed) + ",BAT:" + String(battery);
+    Serial.println(telemetry);
   }
 }
